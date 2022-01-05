1c72463fbc6a5596c805059e3fe8f9da088744bb
Mari Komardina <fox_kom@internet.ru>
2022-01-05 14:49:34 +0300

    feat: function converts the first letter to uppercase

---
+++ task.js
@@ -0,0 +1,16 @@
+/* функция для написания строки с заглавной буквы (содержащей одно слово)*/
+
+function capitalizeWord(word) {
+  let str = '';
+  for (let i = 0; i < word.length; i++) {
+    if(i == 0){
+      str = word[i].toUpperCase();
+    }
+    else {
+      str += word[i];
+    }
+  }
+  return str;
+}
+
+console.log(capitalizeWord("maria"));
