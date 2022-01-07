e63aea94dc3b1673f5c522ad771614326c84a17b
Mari Komardina <fox_kom@internet.ru>
2022-01-07 22:04:01 +0300

    feat: if-else

--- task.js
+++ task.js
@@ -196,3 +196,27 @@ function removeChar(str){
   }
 
   console.log(repeatStr(3, 'Mari'));
+
+
+  /* if-else-if */
+  function finalGrade (exam, projects) {
+    let score;
+    if(exam > 90 || projects > 10){
+      score = 100;
+    } else {
+      if(exam > 75 && projects >= 5) {
+        score = 90;
+      }
+      else {
+        if(exam > 50 && projects >= 2){
+          score = 75;
+        }
+        else {
+          score = 0;
+        }
+      }
+    }
+    return score;
+  }
+
+  console.log(finalGrade(0, 3));
