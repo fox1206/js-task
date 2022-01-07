d683321eb6c371c83bce3ea7b24f954dd236d704
Mari Komardina <fox_kom@internet.ru>
2022-01-07 21:01:18 +0300

    feat: function mult number by eight if it is a even or nine

--- task.js
+++ task.js
@@ -174,3 +174,12 @@ function removeChar(str){
   }
 
   console.log(solution("abcd", "cdp"));
+
+
+  /* Умножении заданного числа на восемь, если оно четное, 
+  и на девять в противном случае. */
+  function simpleMultiplication(number) {
+    return (number % 2 === 0) ? number * 8 : number * 9;
+  }
+
+  console.log(simpleMultiplication(20));
